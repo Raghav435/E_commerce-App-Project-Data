437e2b097700f4cf86e3a8d1413285ac94e8b58d
Raghav435 <shuklaraghav435@gmail.com>
2022-09-02 16:41:45 +0530

    new update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'data.json'));
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
